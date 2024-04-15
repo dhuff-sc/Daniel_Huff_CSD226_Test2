5138b9c25735109b6ba4a07b0b864ec9d4baed83
dhuff-sc <21101634@saultcollege.ca>
2024-04-15 14:51:04 -0400

    spring intialized

---
+++ src/main/java/com/example/demo/DanielHuffCsd226Test2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanielHuffCsd226Test2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanielHuffCsd226Test2Application.class, args);
+	}
+
+}
